5bb5d9f77804719a95f790cfbcb91d35725af771
Lucas André Vasconcelos Souza <lavs1@discente.ifpe.edu.br>
2026-06-11 23:33:30 +0000

    [feat]: created download history

---
+++ draft.py
@@ -0,0 +1,5 @@
+from functions.data import download_history
+
+download_history(
+    "BBAS3.SA"
+)
---
+++ functions/data.py
@@ -0,0 +1,22 @@
+import yfinance as yf
+import pandas as pd
+
+def download_history(
+    ticker:str,
+    multi_level_index:bool = False
+)-> pd.DataFrame:  
+    '''
+    Download historical data from yahoofinance;
+    
+    Args:
+        ticker (str): Ticker Name.
+        multi_level_index (bool): Remove/include multi index.       
+    '''
+    
+
+    df = yf.download(
+        tickers=ticker, 
+        multi_level_index = multi_level_index
+    )
+    
+    return df
